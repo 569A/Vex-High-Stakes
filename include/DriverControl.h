00c05b90ff581f7516a221a3f779ebdf36fbb133
windcolor-dev <79166388+windcolor-dev@users.noreply.github.com>
2024-10-28 21:51:13 -0700

    Move driver control loop to its own file to avoid messiness

---
+++ include/DriverControl.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "main.h"
+#include "ControllerBase.h"
+#include "Ticker.h"
+#include "Recording.h"
+#include "Recorder.h"
+#include "SlewRateController.h"
+
+class DriverControl
+{
+private:
+    Ticker driveTicker = Ticker(20);
+    bool recordingInput = false;
+    Recording recording = Recording();
+    Recorder recorder = Recorder(master, recording);
+    int decreasedSensitivityFactor = 3;
+    int velocity = 600;
+    SlewRateController leftDriveSlewRateController = SlewRateController(60);
+    SlewRateController rightDriveSlewRateController = SlewRateController(60);
+    bool manual = false;
+    bool intakeAvailable = true;
+    bool competitionMode = false;
+    int stage = 1;
+    int ticksSinceIntakeCandidateDetected = 0;
+    int ticksPerIntakeCheck = 4;
+    bool intakeCandidateDetected = false;
+
+    ControllerBase &master;
+    okapi::MotorGroup &leftMotors;
+    okapi::MotorGroup &rightMotors;
+    pros::Motor &intake;
+    pros::ADIDigitalOut &pistonA;
+    pros::ADIDigitalOut &pistonB;
+    pros::Optical &optical;
+    pros::Distance &distanceSensor;
+public:
+    DriverControl(ControllerBase &controller, okapi::MotorGroup &leftMotors, okapi::MotorGroup &rightMotors, pros::Motor &intake, pros::ADIDigitalOut &pistonA, pros::ADIDigitalOut &pistonB, pros::Optical &optical, pros::Distance &distanceSensor);
+    void run();
+    void setCompetitonMode(bool mode) {
+        competitionMode = mode;
+    }
+};
