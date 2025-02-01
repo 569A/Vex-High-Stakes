bc6ce8e75c70ff099075ee6b5a101b1349cf3785
569A <4fh2o42l@duck.com>
2025-01-31 16:15:14 -0800

    Make arm higher, re enable flex wheel on arm lower

--- src/main.cpp
+++ src/main.cpp
@@ -810,7 +810,7 @@ void opcontrol() {
         if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP) && !digital_up_was_pressed) {
             get_arm_ready_to_score = false;
             if (arm_state == 0) {
-                arm_motor.move_absolute(765, 100);
+                arm_motor.move_absolute(795, 100);
                 arm_state++;
             } else if (arm_state == 1) {
                 hook_intake.move_relative(-700, 200);
@@ -824,11 +824,12 @@ void opcontrol() {
         // Down arrow does the opposite.
         if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) && !digital_down_was_pressed) {
            get_arm_ready_to_score = false;
+           flex_wheel_intake.move(-500);
             if (arm_state == 1) {
                 arm_motor.move_absolute(0, 80);
                 arm_state--;
             } else if (arm_state == 2) {
-                arm_motor.move_absolute(765, 100);
+                arm_motor.move_absolute(795, 100);
                 arm_state--;
             }
             digital_down_was_pressed = true;
